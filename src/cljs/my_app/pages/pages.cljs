5b96b5ba287f62d25cca0d97bc00a2ff88daa975
Andrew Shewring <andrew.shewring@s23m.com>
2018-05-13 13:53:29 +1200

    Formatting

--- src/cljs/my_app/pages/pages.cljs
+++ src/cljs/my_app/pages/pages.cljs
@@ -1,32 +1,27 @@
 (ns my-app.pages
   (:require [clojure.string :as s]))
 
-(def ^:private page-metadata
-  {
-    :home
-      {
-        :header "Business Performance Optimisation"
-        :description "S23M makes sense of the world's information from your unique point of view. We offer business performance optimisation services."
-        :button-title "Home"
-      }
-    :about
-      {
-        :header "About"
-        :description "S23M is built around a powerful methodology for gaining a deep understanding of your business and its future potential."
-        :button-title "About"
-      }
-    :life
-      {
-        :header "Collaboration for Life"
-        :description "S23M provides customers with unique expertise in managing complex organisations and systems, and in designing adaptive and resilient systems."
-        :button-title "Collaboration"
-      }
-    :not-found
-      {
-        :header "Page not found"
-        :description "Page not found"
-      }
-  })
+(def ^:private page-metadata {
+  :home {
+    :header "Business Performance Optimisation"
+    :description "S23M makes sense of the world's information from your unique point of view. We offer business performance optimisation services."
+    :button-title "Home"
+  }
+  :about {
+    :header "About"
+    :description "S23M is built around a powerful methodology for gaining a deep understanding of your business and its future potential."
+    :button-title "About"
+  }
+  :life {
+    :header "Collaboration for Life"
+    :description "S23M provides customers with unique expertise in managing complex organisations and systems, and in designing adaptive and resilient systems."
+    :button-title "Collaboration"
+  }
+  :not-found {
+    :header "Page not found"
+    :description "Page not found"
+  }
+})
 
 (defn- lookup-page-metadata
   [id k]
